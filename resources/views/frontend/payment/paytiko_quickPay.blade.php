4a4af586827cee0d38e59c1fcbffb74888b352f8
alaaqutfa <alaaqutfa.work@gmail.com>
2025-05-14 13:52:48 +0300

    Edit design

--- resources/views/frontend/payment/paytiko_quickPay.blade.php
+++ resources/views/frontend/payment/paytiko_quickPay.blade.php
@@ -4,6 +4,40 @@
     <title>Paytiko Payment</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="_token" content="{{ csrf_token() }}">
+    <meta name="description" content="@yield('meta_description', get_setting('meta_description'))" />
+    <meta name="keywords" content="@yield('meta_keywords', get_setting('meta_keywords'))">
+    @if (!isset($detailedProduct) && !isset($customer_product) && !isset($shop) && !isset($page) && !isset($blog))
+        @php
+            $meta_image = uploaded_asset(get_setting('meta_image'));
+        @endphp
+        <!-- Schema.org markup for Google+ -->
+        <meta itemprop="name" content="{{ get_setting('meta_title') }}">
+        <meta itemprop="description" content="{{ get_setting('meta_description') }}">
+        <meta itemprop="image" content="{{ $meta_image }}">
+
+        <!-- Twitter Card data -->
+        <meta name="twitter:card" content="product">
+        <meta name="twitter:site" content="@publisher_handle">
+        <meta name="twitter:title" content="{{ get_setting('meta_title') }}">
+        <meta name="twitter:description" content="{{ get_setting('meta_description') }}">
+        <meta name="twitter:creator" content="@author_handle">
+        <meta name="twitter:image" content="{{ $meta_image }}">
+
+        <!-- Open Graph data -->
+        <meta property="og:title" content="{{ get_setting('meta_title') }}" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ route('home') }}" />
+        <meta property="og:image" content="{{ $meta_image }}" />
+        <meta property="og:description" content="{{ get_setting('meta_description') }}" />
+        <meta property="og:site_name" content="{{ env('APP_NAME') }}" />
+        <meta property="fb:app_id" content="{{ env('FACEBOOK_PIXEL_ID') }}">
+    @endif
+    <!-- Favicon -->
+    @php
+        $site_icon = uploaded_asset(get_setting('site_icon'));
+    @endphp
+    <link rel="icon" href="{{ $site_icon }}">
+    <link rel="apple-touch-icon" href="{{ $site_icon }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Font Awesome CDN -->
